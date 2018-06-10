7dfeefa7fbd525eeb0a4fa777f6fc42349db1917
John Mayer <john.p.mayer.jr@gmail.com>
2018-06-09 20:42:30 -0400

    bolt prefab

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody rb;
+
+    // executed on first frame when object instantiated
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        rb.velocity = transform.forward * speed;
+    }
+}
