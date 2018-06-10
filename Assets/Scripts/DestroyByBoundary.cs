0e16861c4a417ca28c3d5a0eed8cfc9047948654
John Mayer <john.p.mayer.jr@gmail.com>
2018-06-09 21:32:41 -0400

    destroy objects out of bounds

---
+++ Assets/Scripts/DestroyByBoundary.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class DestroyByBoundary : MonoBehaviour
+{
+    // called when the other collider stops touching this object
+    // in our game, it's when objects leave the object boundary
+    private void OnTriggerExit(Collider other)
+    {
+        Destroy(other.gameObject);
+    }
+}
