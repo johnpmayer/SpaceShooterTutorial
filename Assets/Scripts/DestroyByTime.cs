47bbc0b1c9ca242da490c60cbbf333346c59458e
John Mayer <john.p.mayer.jr@gmail.com>
2018-06-09 22:29:24 -0400

    spawn waves and explosion cleanup

---
+++ Assets/Scripts/DestroyByTime.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class DestroyByTime : MonoBehaviour
+{
+    public float lifetime;
+
+    private void Start()
+    {
+        Destroy(gameObject, lifetime);
+    }
+}
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,19 +1,34 @@
-﻿using UnityEngine;
+﻿using System.Linq;
+using UnityEngine;
+using System.Collections;
 
 public class GameController : MonoBehaviour
 {
     public GameObject hazard;
     public Vector3 spawnValues;
+    public int hazardCount;
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
 
     private void Start()
     {
-        SpawnWaves();
+        StartCoroutine(SpawnWaves());
     }
 
-    private void SpawnWaves()
+    private IEnumerator SpawnWaves()
     {
-        float spawnX = Random.Range(-spawnValues.x, spawnValues.x);
-        Vector3 spawnPosition = new Vector3(spawnX, spawnValues.y, spawnValues.z);
-        Instantiate(hazard, spawnPosition, Quaternion.identity);
+        yield return new WaitForSeconds(startWait);
+        while (true)
+        {
+            foreach (int i in Enumerable.Range(0, hazardCount))
+            {
+                float spawnX = Random.Range(-spawnValues.x, spawnValues.x);
+                Vector3 spawnPosition = new Vector3(spawnX, spawnValues.y, spawnValues.z);
+                Instantiate(hazard, spawnPosition, Quaternion.identity);
+                yield return new WaitForSeconds(spawnWait);
+            }
+            yield return new WaitForSeconds(waveWait);
+        }
     }
 }
