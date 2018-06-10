627cfd90ef86f754ec243cf6e02063962914cdee
John Mayer <john.p.mayer.jr@gmail.com>
2018-06-09 22:13:06 -0400

    spawn random asteroid at game start

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject hazard;
+    public Vector3 spawnValues;
+
+    private void Start()
+    {
+        SpawnWaves();
+    }
+
+    private void SpawnWaves()
+    {
+        float spawnX = Random.Range(-spawnValues.x, spawnValues.x);
+        Vector3 spawnPosition = new Vector3(spawnX, spawnValues.y, spawnValues.z);
+        Instantiate(hazard, spawnPosition, Quaternion.identity);
+    }
+}
