ab16e3184c4b3d1db8768c19cb85a05ebcc92fa6
John Mayer <john.p.mayer.jr@gmail.com>
2018-06-09 21:58:54 -0400

    asteroid prefab, movement, explosions

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -2,6 +2,9 @@
 
 public class DestroyByContact : MonoBehaviour {
 
+    public GameObject explosion;
+    public GameObject playerExplosion;
+
     // called when a collider (other) first touches this collider
     private void OnTriggerEnter(Collider other)
     {
@@ -9,6 +12,14 @@ public class DestroyByContact : MonoBehaviour {
         {
             return;
         }
+
+        Instantiate(explosion, transform.position, transform.rotation);
+
+        if (other.tag == "Player")
+        {
+            Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+        }
+        
         // order in the following two statements doesn't matter, they just
         // "mark" an object to be destroyed at the end of the frame
         Destroy(other.gameObject);
